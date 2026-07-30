6d75ec92910cb2ee739af829ec4c4e6cdf098460
wjlin24207 <wjlin24207@gmail.com>
2026-07-30 10:10:25 +0800

    Update 1_台股.py

--- pages/1_台股.py
+++ pages/1_台股.py
@@ -170,7 +170,7 @@ def process_kd_logic(stock_id, live_info, hists_all):
         return None
 
 # ===== 4. 主程式資料流準備 =====
-watchlist_stocks = ["^TWII", "0056", "00878", "00919", "0050", "00981A", "00988A", "00631L", "2330", "3711"]
+watchlist_stocks = ["^TWII", "0056", "00878", "00919", "0050", "009816", "00981A", "00988A", "00631L", "2330", "3711"]
 
 prices = get_all_live_prices(watchlist_stocks)
 hists = get_all_yahoo_hist(watchlist_stocks)
